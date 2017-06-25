23c4a13b213fc1fd7f415aec3b2e47180d21739b
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-06-25 20:23:10 +0900

    update build script

--- 2.0/build.sh
+++ 2.0/build.sh
@@ -1,3 +1,3 @@
 image_name=kokeiro001/rpi-dotnet
-docker build -t $image_name:2.0-runtime-deps  ./runtime-deps/jessie/
-docker build -t $image_name:2.0-runtime  ./runtime/jessie/
+docker build --no-cache=true -t $image_name:2.0-runtime-deps  ./runtime-deps/jessie/
+docker build --no-cache=true -t $image_name:2.0-runtime  ./runtime/jessie/
